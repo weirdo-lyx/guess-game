c325fe28ee443ce52d8ef15f0bb7861f147472cd
lyx <1059777041@qq.com>
2025-06-09 23:34:33 +0800

    add

---
+++ pkg/simple_guess_number_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start_game: () => number;
+export const check_guess: (a: number, b: number) => [number, number];
+export const __wbindgen_exn_store: (a: number) => void;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
